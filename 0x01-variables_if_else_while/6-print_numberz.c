e79055b32853b833dc8e8c28e7a295eff4d7fe72
Bloomkal <m.mulukal2010@gmail.com>
2023-02-19 12:46:44 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints nymbers 0-9 without using char or printf/puts,
+ * and putchar 2x max
+(*
+* Return: 0 on success
+*/
+int main(void)
+{
+  int I = '0';
+
+  while (I <= '9')
+    {
+      putchar(i);
+      i++;
+    }
+  putchar('\n');
+  return (0);
+}

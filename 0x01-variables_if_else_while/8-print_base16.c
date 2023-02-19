16090ff2a99e8bc69731bbd014fa713fb0fdb677
Bloomkal <m.mulukal2010@gmail.com>
2023-02-19 13:23:15 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers of base 16.
+(*
+* Return: 0 on success
+*/
+int main(void)
+{
+  int i= '0';
+
+  while (I <= '9')
+    {
+      putchar(i);
+      i++;
+    }
+  I = 'a';
+  while (I <= 'f')
+    {
+      putchar(i);
+      i++;
+    }
+  putchar('\n');
+  return (0);
+
+}

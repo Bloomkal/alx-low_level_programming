f01add469964b432781a0f6686b2800e37d57202
Bloomkal <m.mulukal2010@gmail.com>
2023-02-19 22:45:33 -0800

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 00 to 99.
+(*
+* Return: 0 on success
+*/
+int main(void)
+{
+  int I = '0';
+  int j = '0';
+
+  while (I <= '9')
+    {
+      while (j <= '9')
+	{
+	  if (!(I > j || I == j))
+	    {
+	      putchar(i);
+	      putchar(j);
+	      if (I == '8' && j == '9')
+		{
+		  putchar('\n');
+		}
+	      else
+		{
+		  putchar(',');
+		  putchar(' ');
+		}
+	    }
+	  j++;
+	}
+      j = '0';
+      i++;
+    }
+  return (0);
+}

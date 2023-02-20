375de7aeff968c89d685d51c41322e169464b1de
Bloomkal <123313265+Bloomkal@users.noreply.github.com>
2023-02-20 13:18:38 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,25 +12,25 @@ int j = '0';
 while (i <= '9')
 {
 while (j <= '9')
-	{
-	if (!(i > j || i == j))
-	    {
-	      putchar(i);
-	      putchar(j);
-	      if (i == '8' && j == '9')
-		{
-		  putchar('\n');
-		}
-	      else
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	    }
-	  j++;
-	}
-      j = '0';
-	i++;
-    }
-  return (0);
+{
+if (!(i > j || i == j)
+{
+putchar(i);
+putchar(j);
+if (i == '8' && j == '9')
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+j++;
+}
+j = '0';
+i++;
+}
+return (0);
 }

9eebe1cb75f931164f2d16a3e7e7a4b7d245f532
Bloomkal <123313265+Bloomkal@users.noreply.github.com>
2023-02-20 15:08:30 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,42 +1,42 @@
-#3include <stdio.h>
+#include <stdio.h>
 /**
- * main - prints all possible combinations of three different digits,
- * in ascending order, separated by a comma followed by a space.
+* main - prints all possible combinations of three different digits,
+* in ascending order, separated by a comma followed by a space.
 (*
- * Return: 0 on success
- */
- int main(void)
- {
-   int I = '0';
-   int j = '0';
-   int k = '0';
+* Return: 0 on success
+*/
+int main(void)
+{
+int i = '0';
+int j = '0';
+int k = '0';
 
-   while (I <= '7')
-     {
-       while (j <= '8')
-	 {
-	   while (k <= '9')
-	     {
-	       if (I < j && j < k)
-		 {
-		   putchar(i);
-		   putchar(j);
-		   putchar(k);
-		   if (!(I == '7' && j == '8' && k == '9'))
-		     {
-		       putchar(',');
-		       putchar(' ');
-		     }
+while (i <= '7')
+{
+while (j <= '8')
+	{
+	while (k <= '9')
+	{
+	if (i < j && j < k)
+		{
+		putchar(i);
+		putchar(j);
+		putchar(k);
+		if (!(i == '7' && j == '8' && k == '9'))
+		{
+		putchar(',');
+		putchar(' ');
+		}
 
-		 }
-	       k++;
-	     }
-	   k = '0';
-	   j++;
-	 }
-       j = '0';
-       i++;
-     }
-   putchar('\n');
-   return (0);
- }
+		}
+	k++;
+	}
+	k = '0';
+	j++;
+	}
+	j = '0';
+	i++;
+}
+	putchar('\n');
+return (0);
+}

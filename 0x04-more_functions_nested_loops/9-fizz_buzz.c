d18cd1f9e1e7bd8f3df46d2249b502572f43157e
Bloomkal <123313265+Bloomkal@users.noreply.github.com>
2023-02-24 00:37:24 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,15 +8,15 @@
   */
 int main(void)
 {
-	int x;
-  
+int x;
+
 for (x = 1; x <= 100; x++)
-	{
-		if ((x % 3 == 0) && (x % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if (x % 3 == 0)
+{
+if ((x % 3 == 0) && (x % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if (x % 3 == 0)
 {
 printf("Fizz");
 }
@@ -36,6 +36,6 @@ printf(" ");
 }
 
 printf("\n");
-  
+
 return (0);
 }
